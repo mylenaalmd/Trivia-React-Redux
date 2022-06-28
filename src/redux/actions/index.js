ac3be4999810be6d217a301ed8b5f5419328afbf
GusCabraal <gustavocabral97@hotmail.com>
2022-06-28 16:28:51 -0300

    configuração do redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,22 @@
+export const SAVE_NAME = 'SAVE_NAME';
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_ASSERTIONS = 'SAVE_ASSERTIONS';
+export const SAVE_SCORE = 'SAVE_SCORE';
+
+export const saveName = (name) => ({
+  type: SAVE_NAME,
+  payload: name,
+});
+
+export const saveEmail = (email) => ({
+  type: SAVE_EMAIL,
+  payload: email,
+});
+export const saveAssertions = (assertions) => ({
+  type: SAVE_ASSERTIONS,
+  payload: assertions,
+});
+export const saveScore = (score) => ({
+  type: SAVE_SCORE,
+  payload: score,
+});
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,51 @@
+import { combineReducers } from 'redux';
+import {
+  SAVE_EMAIL,
+  SAVE_NAME,
+  SAVE_ASSERTIONS,
+  SAVE_SCORE,
+} from '../actions';
+
+// const INITIAL_STATE = {
+//   name: nomeDaPessoa,
+//   assertions: numeroDeAcertos,
+//   score: pontuacao,
+//   gravatarEmail: emailDaPessoa,
+// };
+const INITIAL_STATE = {
+  name: '',
+  assertions: '',
+  score: 0,
+  gravatarEmail: '',
+};
+
+const exampleReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_NAME:
+    return {
+      ...state,
+      name: action.payload,
+    };
+  case SAVE_EMAIL:
+    return {
+      ...state,
+      gravatarEmail: action.payload,
+    };
+  case SAVE_ASSERTIONS:
+    return {
+      ...state,
+      assertions: action.payload,
+    };
+  case SAVE_SCORE:
+    return {
+      ...state,
+      score: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+const rootReducer = combineReducers({ exampleReducer });
+
+export default rootReducer;
