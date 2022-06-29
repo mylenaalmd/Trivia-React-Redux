03557edc08cde338a7ad384b0430fb38482abb6b
GusCabraal <gustavocabral97@hotmail.com>
2022-06-29 18:42:35 -0300

    resolvido primeiros requisitos

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,27 +4,74 @@ import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 
 class Game extends React.Component {
+  state = {
+    questions: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions = async () => {
+    const token = localStorage.getItem('token');
+    const responseApi = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+      .then((response) => response.json())
+      .then((data) => data.results);
+    console.log(responseApi);
+    this.setState({
+      questions: responseApi,
+      loading: false,
+    });
+  }
+
   getGravatar = () => {
     const { userEmail } = this.props;
     const takeEmail = md5(userEmail).toString();
     const takeAvatar = `https://www.gravatar.com/avatar/${takeEmail}`;
-    console.log(takeAvatar);
+    // console.log(takeAvatar);
     return takeAvatar;
   }
 
   render() {
     const { userName } = this.props;
+    const { questions, loading } = this.state;
+    // console.log(questions);
     return (
-      <header>
-        {console.log(this.getGravatar())}
-        <img
-          src={ this.getGravatar() }
-          alt="gravatar"
-          data-testid="header-profile-picture"
-        />
-        <h3 data-testid="header-player-name">{userName}</h3>
-        <h3 data-testid="header-score">0</h3>
-      </header>
+      <div>
+
+        <header>
+          {/* {console.log(this.getGravatar())} */}
+          <img
+            src={ this.getGravatar() }
+            alt="gravatar"
+            data-testid="header-profile-picture"
+          />
+          <h3 data-testid="header-player-name">{userName}</h3>
+          <h3 data-testid="header-score">0</h3>
+        </header>
+        {loading ? <p>carregando....</p> : (
+          <div>
+            <p data-testid="question-category">{questions[0].category}</p>
+            <p data-testid="question-text">{questions[0].question}</p>
+            <div data-testid="answer-options">
+              <button
+                type="button"
+                data-testid="correct-answer"
+              >
+                {questions[0].correct_answer}
+              </button>
+              {questions[0].incorrect_answers.map((element, index) => (
+                <button
+                  type="button"
+                  data-testid={ `wrong-answer-${index}` }
+                  key={ element }
+                >
+                  {element}
+                </button>))}
+            </div>
+          </div>)}
+      </div>
     );
   }
 }
