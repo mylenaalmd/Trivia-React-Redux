0bc2c1cebb9c287b1674caba7e5187431f6b4ac8
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-07-03 00:38:26 -0300

    requisito 18, implementação de um botão de redirect no componente ranking para o home do app

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  redirectLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <header>
+          <h1 data-testid="ranking-title">Ranking</h1>
+        </header>
+        <section>
+          <button
+            data-testid="btn-go-home"
+            type="button"
+            name="btn-go-home"
+            onClick={ this.redirectLogin }
+          >
+            Login
+          </button>
+        </section>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: propTypes.shape().isRequired,
+};
 export default Ranking;
